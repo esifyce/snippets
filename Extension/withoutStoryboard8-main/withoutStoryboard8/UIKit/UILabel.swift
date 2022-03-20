3f785137362524a6c8c2b08cbc49b18abd5b6fcb
esifyce <49156359+esifyce@users.noreply.github.com>
2022-03-21 03:11:59 +0600

    Add files via upload

---
+++ Extension/withoutStoryboard8-main/withoutStoryboard8/UIKit/UILabel.swift
@@ -0,0 +1,18 @@
+//
+//  UILabel.swift
+//  withoutStoryboard8
+//
+//  Created by Sabir Myrzaev on 3/3/22.
+//
+
+import UIKit
+
+extension UILabel {
+    
+    
+    convenience init(text: String) {
+        self.init()
+        self.text = text
+
+    }
+}
